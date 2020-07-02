f6d3444f63cfcc7f7b864b7a26c899927488da7f
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2020-07-02 09:17:13 +0200

    implement a simple UDP server

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,71 @@
 use tracing::{info, instrument, Level};
 use tracing_subscriber;
+use tokio;
+use tokio::net::UdpSocket;
+use std::error::Error;
+use std::net::SocketAddr;
+use std::{env, io};
+use tracing_futures;
 
-fn main() {
+#[derive(Debug)]
+struct Server {
+    socket: UdpSocket,
+    buf: Vec<u8>,
+    to_send: Option<(usize, SocketAddr)>,
+}
+
+impl Server {
+    #[instrument(level="trace", skip(self))]
+    async fn run(self) -> Result<(), io::Error> {
+        info!("Start server");
+        let Server {
+            mut socket,
+            mut buf,
+            mut to_send,
+        } = self;
+
+        loop {
+            // First we check to see if there's a message we need to echo back.
+            // If so then we try to send it back to the original source, waiting
+            // until it's writable and we're able to do so.
+            if let Some((size, peer)) = to_send {
+                let amt = socket.send_to(&buf[..size], &peer).await?;
+
+                info!("Echoed {}/{} bytes to {}", amt, size, peer);
+            }
+
+            // If we're here then `to_send` is `None`, so we take a look for the
+            // next message we're going to echo back.
+            to_send = Some(socket.recv_from(&mut buf).await?);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Tracing subscriber
     let subscriber = tracing_subscriber::fmt()
         .json()
         .with_max_level(Level::TRACE)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("no global subscriber has been set");
 
-    info!("This will be logged to stdout");
-    test_func("Nicolas");
-}
+    // Start local server
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+    
+    let socket = UdpSocket::bind(&addr).await?;
+    info!("Listening on: {}", socket.local_addr()?);
 
-#[instrument]
-fn test_func(name: &str) {
-    info!("Got name {}", name);
-    println!("Hello, {}!", name);
+    let server = Server {
+        socket,
+        buf: vec![0; 1024],
+        to_send: None,
+    };
+
+    // This starts the server task.
+    server.run().await?;
+
+    Ok(())
 }
